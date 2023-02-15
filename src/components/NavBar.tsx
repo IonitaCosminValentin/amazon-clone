2a4886a1fd0dcabf9b4298ec5fa307f96920924b
IonitaCosminValentin <cosminvalentinionita@gmail.com>
2023-02-15 18:08:22 +0200

    Update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import amazon from '../assets/amazon.png'
+import { HiOutlineLocationMarker } from 'react-icons/hi'
 
 const NavBar = () => {
   return (
-    <div className='nav-bar'>
-      <div className='nav-left'>
-        <img src={amazon} alt='' className='logo' />
+    <div className='nav-container'>
+      <div className='nav-bar'>
+        <div className='nav-left'>
+          <img src={amazon} alt='' className='logo' />
+          <div className='deliver-div'>
+            <div id='location-icon-container'>
+              <HiOutlineLocationMarker id='location-icon' />
+            </div>
+            <div>
+              Deliver to <span>Romania</span>
+            </div>
+          </div>
+        </div>
+        <div className='search-form'>
+          <option></option>
+          <input type='text' />
+          <button></button>
+        </div>
       </div>
+      <div className='under-nav'></div>
     </div>
   )
 }
